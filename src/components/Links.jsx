b179789c87722faca6e07277248a922af6263a44
Emanuele Biancardi <emanuele.biancardi@wyscout.com>
2016-12-18 16:23:17 +0100

    Update AS calculator link

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -6,7 +6,7 @@ export default function Links()  {
                 <li><a href="http://www.clickerheroes.com/" target="_blank">http://www.clickerheroes.com/</a> (the game)</li>
                 <li><a href="https://www.reddit.com/r/ClickerHeroes/" target="_blank">https://www.reddit.com/r/ClickerHeroes/</a> (the subreddit)</li>
                 <li><a href="http://clickerheroes.wikia.com/" target="_blank">http://clickerheroes.wikia.com/</a> (the wikia)</li>
-                <li><a href="https://jsfiddle.net/graceoflives/2b7am8Lg/show/" target="_blank">https://jsfiddle.net/graceoflives/2b7am8Lg/show/</a> (ancients calculator)</li>
+                <li><a href="https://graceoflives.github.io/fluffy-garbanzo/" target="_blank">https://graceoflives.github.io/fluffy-garbanzo/</a> (ancients calculator)</li>
                 <li><a href="https://docs.google.com/spreadsheets/d/1m09HoNiLW-7t96gzguG9tU_HHaRrDrtMpAoAuukLB4w/edit#gid=233428405" target="_blank">
                     https://docs.google.com/spreadsheets/d/1m09HoNiLW-7t96gzguG9tU_HHaRrDrtMpAoAuukLB4w/edit#gid=233428405
                 </a> (outsiders spreadsheet)</li>
