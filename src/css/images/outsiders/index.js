d5c3cc227a8051edd5d7f7a24796c64053ba6876
Emanuele Biancardi <emanuele.biancardi@wyscout.com>
2017-11-26 22:41:44 +0100

    Add missing outsiders placeholder

--- src/css/images/outsiders/index.js
+++ src/css/images/outsiders/index.js
@@ -3,10 +3,10 @@ import Chorgorloth from './Chor.png';
 import Phandoryss from './Phan.png';
 import Ponyboy from './Ponyboy.png';
 import Borb from './Borb.png';
-import Rhageist from './Borb.png';
-import KAriqua from './Borb.png';
-import Orphalas from './Borb.png';
-import SenAkhan from './Borb.png';
+import Rhageist from './Placeholder.png';
+import KAriqua from './Placeholder.png';
+import Orphalas from './Placeholder.png';
+import SenAkhan from './Placeholder.png';
 
 export default {
     Xyliqil,
